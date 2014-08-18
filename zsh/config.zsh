f5577da927a018bdc582235499d04e8e4670cb96
shawa <andrewshaw.mail@gmail.com>
2014-08-18 20:52:21 +0100

    vim is the pager

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,7 +6,12 @@ fi
 
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
-#export LC_CTYPE='C'
+
+#wow
+export PAGER="/bin/sh -c \"unset PAGER;col -b -x | \
+    vim -R -c 'set ft=man nomod nolist' -c 'map q :q<CR>' \
+    -c 'map <SPACE> <C-D>' -c 'map b <C-U>' \
+    -c 'nmap K :Man <C-R>=expand(\\\"<cword>\\\")<CR><CR>' -\""
 
 fpath=($ZSH/functions $fpath)
 
