821e09b58f6200714eedf77b2b60ff13ead80d4d
shawa <andrewshaw.mail@gmail.com>
2015-05-27 14:02:04 +0100

    Add pbcopy aliases if we're on a Linux box

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,14 @@ alias cal='cal -y'
 alias yt='youtube-dl'
 alias tx='tmux a -d || (cd; tmux)'
 alias qr='qrencode -t ANSI'
+
+
+if [ $(uname) = 'Linux' ];
+then
+    alias pbcopy='xsel --clipboard --input'
+    alias pbpaste='xsel --clipboard --output'
+fi
+
 mkcd() {
     mkdir -p -- "$1" &&
     cd -P -- "$1"
