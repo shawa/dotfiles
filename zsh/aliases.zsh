6923c1d9af25d844c846f01427e2c90b33b662a6
andrew-shaw <shawa1@tcd.ie>
2016-04-13 22:41:14 +0100

    remove legacy alias file

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,6 @@
 alias reload!='. ~/.zshrc'
 alias cal='cal -y'
-alias yt='youtube-dl'
-alias t='tmux a -d || (cd; tmux)'
+alias tx='tmux a -d || (cd; tmux)'
 alias qr='qrencode -t ANSI'
 alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome 2> /dev/null > /dev/null &'
 alias cmus='/usr/local/bin/cmus 2> /dev/null' # suppress the complaints about Audo Units
@@ -9,10 +8,18 @@ alias urm='/bin/rm'
 alias prolog='swipl'
 alias gcc='/usr/local/Cellar/gcc/5.3.0/bin/gcc-5'
 
+if $(gls &>/dev/null)
+then
+  alias ls="gls --color -F"
+  alias l="gls --color -lAh"
+  alias ll="gls --color -l"
+  alias la="gls --color -A"
+fi
+
 rm(){ srm $@ & }
 
 remote() {
-    [ -z $1 ] && return 
+    [ -z $1 ] && return
     local URL=$(ssh cube "/home/shawa/.dotfiles/bin/repo $1")
     pbcopy <<< $URL
     echo $URL
@@ -25,6 +32,11 @@ tip() {
     git push
 }
 
+mkcd() {
+    mkdir -p -- "$1" &&
+    cd -P -- "$1"
+}
+
 if [ $(uname) = 'Linux' ];
 then
     alias ls='ls --color=auto'
@@ -32,7 +44,4 @@ then
     alias pbpaste='xsel --clipboard --output'
 fi
 
-mkcd() {
-    mkdir -p -- "$1" &&
-    cd -P -- "$1"
-}
+
